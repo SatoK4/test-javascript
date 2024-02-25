105b71c49af5fba8cee7c3e3bb74a6005d6c1bd3
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2024-02-25 12:03:59 +0000

    Study 再宣言について学習

---
+++ practice-variable/sample.js
@@ -0,0 +1,22 @@
+// -----varによる再宣言-----
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+// 問題ないことを確認
+
+// -----letによる再宣言-----
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+// 「nickname」という変数は既に宣言されているというエラーが発生。
+// 再宣言はできない
+
+// -----constによる再宣言-----
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+// 「let」と同様のエラーが出る
+// 再宣言はできない
