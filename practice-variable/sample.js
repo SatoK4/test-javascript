810fd828393843691daf0f5cf67bd3e7603e2643
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2024-02-25 12:15:20 +0000

    Study 再代入について学習

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -23,3 +23,30 @@
 // 再宣言はできない
 
 // 再宣言は「var」のみ可
+
+
+// -----再代入-----
+// 変数に値を代入した後に、別の値を代入すること
+
+// ・varによる再代入
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+// 問題ないことを確認
+
+// ・letによる再代入
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+// 問題ないことを確認
+
+// ・constによる再代入
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+// 「定数とした変数には割り当てできない（nicknameにはjiroを代入できない）」エラーが発生
+
+// 再代入は「var」と「let」が可
